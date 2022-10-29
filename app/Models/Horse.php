7b43aa14d1f4684e4e8a5e570f9dc6efc6055825
Sergio <sergiofdezp7@gmail.com>
2022-10-29 20:13:23 +0200

    First commit

---
+++ app/Models/Horse.php
@@ -0,0 +1,12 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class Horse extends Model
+{
+    use HasFactory;
+    protected $fillable = ['nombre', 'descripcion', 'imagen', 'raza', 'edad', 'altura', 'doma'];
+}
---
+++ database/migrations/2022_10_29_174806_create_horses_table.php
@@ -0,0 +1,38 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateHorsesTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('horses', function (Blueprint $table) {
+            $table->id();
+            $table->string('nombre');
+            $table->string('descripcion');
+            $table->string('imagen');
+            $table->string('raza');
+            $table->string('edad');
+            $table->string('altura');
+            $table->string('doma');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('horses');
+    }
+}
