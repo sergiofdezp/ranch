a667bd7b33835f866813c5c8e8960bd027ca0ed0
Sergio <sergiofdezp7@gmail.com>
2022-12-06 15:27:04 +0100

    Changes in contact

--- resources/views/contact/email.blade.php
+++ resources/views/contact/email.blade.php
@@ -3,10 +3,10 @@
     <div class="py-12">
         <div class="max-w-7xl mx-auto sm:px-6 lg:px-8">
             <div class="bg-white overflow-hidden shadow-xl sm:rounded-lg">
-                <div class="flex items-center justify-center p-12">
-                    <div class="map">
+            <div class="map">
                         <iframe class="w-full text-center" src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d23202.87212814502!2d-5.866167370916516!3d43.36951671151896!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0xd368c9a60ac1c67%3A0x3134440ecc5e6224!2sOviedo%2C%20Asturias!5e0!3m2!1ses!2ses!4v1669465147664!5m2!1ses!2ses" width="600" height="450" style="border:0;" allowfullscreen="" loading="lazy" referrerpolicy="no-referrer-when-downgrade"></iframe>
                     </div>
+                <div class="flex items-center justify-center p-12">
                     <div class="mx-auto w-full max-w-[550px]">
                         @if(session('message'))
                             <div class="alert alert-success">{{session('message')}}</div>
